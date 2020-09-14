9a91434771e5d19b88cc5bbde51b120b205d44cb
yoel <yoel@macbook-pro.home>
2020-09-14 14:55:10 -0700

    [fizzbuzz] add (100)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,16 @@
 def fizzbuzz(n):
-    for i in range(1, n+1):
-        if i % 3 == 0 and i % 5 == 0:
-            print((f'{i} % 3 == 0', 'fizzbuzz'))
-        elif i % 3 == 0:
-            print((f'{i} % 3 == 0', 'fizz'))
+    for i in range(1, n + 1):
+        if i % 15 == 0:
+            tuple_one = (f'{i} % 15 == 0', 'FizzBuzz')
+            print(tuple_one)
         elif i % 5 == 0:
-            print((f'{i} % 3 == 0', 'buzz'))
+            tuple_two = (f'{i} % 5 == 0', 'Buzz')
+            print(tuple_two)
+        elif i % 3 == 0:
+            tuple_three = (f'{i} % 3 == 0', 'Fizz')
+            print(tuple_three)
         else:
             print(i)
 
 
-fizzbuzz(20)
+fizzbuzz(100)
